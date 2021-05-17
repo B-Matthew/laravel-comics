a466a78ba307cf25dd072d34a39c30302bce618a
Matthew <matsgeco92@hotmail.it>
2021-05-17 20:28:29 +0200

    Esercizio completo

--- lavarel-comics/resources/views/components/footer.blade.php
+++ lavarel-comics/resources/views/components/footer.blade.php
@@ -1,49 +1,62 @@
 <footer>
   <section class="footer-top">
     <div class="container">
-      <div class="lists">
-        <ul>
-          <li><h3>DC COMICS</h3></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><h3>SHOP</h3></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-        </ul>
-        <ul>
-
-          <li><h3>DC</h3></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-        </ul>
-        <ul>
-          <li><h3>SITES</h3></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Characters</a></li>
-        </ul>
-      </div>
+      <div class="dcImg">
+        <div class="lists">
+          <ul>
+            <li><h3>DC COMICS</h3></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+            <li><h3>SHOP</h3></li>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+          <ul>
+            <li><h3>DC</h3></li>
+            <li><a href="#">Term of Use</a></li>
+            <li><a href="#">Privacy Policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+          <ul>
+            <li><h3>SITES</h3></li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+      <p>All Site Content TM and &#169; 2020 DC Entertainment, unless otherwise <a class="blue" href="#">noted here</a>. All rights reserved.</p>
+      <p><a class="blue" href="#">Cookies Settings</a></p>
+    </div>
     </div>
   </section>
   <section class="footer-bot">
-
-
-
+    <div class="container">
+      <div>
+        <button type="button" name="button">SIGN-UP NOW!</button>
+      </div>
+      <div class="social">
+        <a href="#">FOLLOW US</a>
+        <img src="{{ asset('/storage/images/footer-facebook.png') }}" alt="">
+        <img src="{{ asset('/storage/images/footer-twitter.png') }}" alt="">
+        <img src="{{ asset('/storage/images/footer-youtube.png') }}" alt="">
+        <img src="{{ asset('/storage/images/footer-pinterest.png') }}" alt="">
+        <img src="{{ asset('/storage/images/footer-periscope.png') }}" alt="">
+      </div>
+    </div>
   </section>
 </footer>
